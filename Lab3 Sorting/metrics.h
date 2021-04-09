400ff65a3b508546ee5727b0fc122834acec54f1
Rendel Abrasia <67211803+rendelabrasia@users.noreply.github.com>
2021-04-09 15:32:13 -0400

    Create metrics.h

---
+++ Lab3 Sorting/metrics.h
@@ -0,0 +1,11 @@
+#ifndef METRICS_H
+#define METRICS_H
+
+int myCompare(int, int);
+void mySwap(int *, int *);
+void myCopy(const int *, int *);
+unsigned int getNumCompares();
+unsigned int getNumCopies();
+unsigned int getNumSwaps();
+
+#endif /* #ifndef METRICS_H */
